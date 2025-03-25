98e684252540ddcdc3d9963e09403f0f28292123
getsoss <getsoss@naver.com>
2025-03-25 13:28:43 +0900

    [style] 터미널, 기본 배경 스타일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"; // 클라이언트 전용 코드로 표시
 
 import dynamic from "next/dynamic";
+import "./globals.css";
 
 // 클라이언트 사이드에서만 로드되도록 설정
 const Terminal = dynamic(() => import("../app/components/Terminal"), {
@@ -9,7 +10,7 @@ const Terminal = dynamic(() => import("../app/components/Terminal"), {
 
 export default function TerminalPage() {
   return (
-    <div>
+    <div className="bg-blue-100 h-screen w-screen p-4">
       <Terminal />
     </div>
   );
