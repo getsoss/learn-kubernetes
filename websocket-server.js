b2b3561a0a2ffb1b3f3c20e4e516555e2fb0baca
MBH992 <ymca66@naver.com>
2025-10-02 09:43:54 +0900

    [fix] zsh bash로 수정

--- websocket-server.js
+++ websocket-server.js
@@ -3,7 +3,11 @@ const pty = require("node-pty");
 const os = require("os");
 
 // 플랫폼에 맞는 shell 지정
-const shell = os.platform() === "win32" ? "powershell.exe" : "zsh";
+const shell = process.env.SHELL
+  ? process.env.SHELL
+  : os.platform() === "win32"
+  ? "powershell.exe"
+  : "/bin/bash";
 
 const wss = new WebSocket.Server({ port: 8889 });
 
